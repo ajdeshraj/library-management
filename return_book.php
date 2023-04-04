ba2754c26042a0e40806b284f1d16cac235829e0
prahlad-anand <106548901+prahlad-anand@users.noreply.github.com>
2023-04-05 02:04:23 +0530

    Ratings added

--- return_book.php
+++ return_book.php
@@ -27,11 +27,11 @@
             $result = $stmt->get_result();
             if($result->num_rows>0) 
             {
-                echo "<table><tr><th>Book ID</th><th>Name</th><th>Author</th><th>Rating</th><th>Borrower ID</th></tr>";
+                echo "<table><tr><th>Book ID</th><th>Name</th><th>Author</th><th>Rating</th><th>Borrower ID</th><th>Number of times Borrowed</th></tr>";
                 while($row = $result->fetch_assoc())
                 {
                     echo "<tr>";
-                    echo "<td>".$row["book_id"]."</td>"."<td>".$row["book_name"]."</td>"."<td>".$row["author"]."</td>"."<td>".$row["avg_rating"]."</td>"."<td>".$row["borrower_id"]."</td>";
+                    echo "<td>".$row["book_id"]."</td>"."<td>".$row["book_name"]."</td>"."<td>".$row["author"]."</td>"."<td>".$row["avg_rating"]."</td>"."<td>".$row["borrower_id"]."</td><td>".$row["num_borrowed"]."</td>";
                     echo "</tr>";
                 }
             }
@@ -40,42 +40,12 @@
         <form method="post" onsubmit="return validate();"> 
             <input type="text" required name="book_id" id="book_id" placeholder="Enter Book ID of book to return">
             <label for="book_id" id="book_id_msg"></label>
+            <br>
             <input type="text" required name="rating" id="rating" placeholder="Enter rating for the book from 1 to 10">
             <label for="rating" id="rating_msg"></label>
+            <br>
             <input type="submit" value="Return" name="submit">
         </form>
-        <script>
-            function validate()
-            {
-                const rating = document.getElementById("rating").value;
-                if((!rating<=10) || (!rating>=1))
-                {
-                    document.getElementById("username_msg").innerHTML="Invalid username";
-                    document.getElementById("username_msg").style.color=red;
-                    wait(1000);
-                    return false;
-                }
-
-                // Password Validation
-                const pw_pat = /(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*\W).{8,20}/
-                if(!password1.match(pw_pat))
-                {
-                    document.getElementById("password1_msg").innerHTML="Invalid Password";
-                    document.getElementById("password1_msg").style.color=red;
-                    wait(1000);
-                    return false;
-                }
-                else if(!(password1===password2))
-                {
-                    document.getElementById("password1_msg").innerHTML="Passwords do not match";
-                    document.getElementById("password1_msg").style.color=red;
-                    wait(1000);
-                    return false;
-                }
-            
-                return true;
-            }          
-        </script>
         <?php
             if(isset($_POST['submit']))
             {
@@ -91,23 +61,26 @@
                 }
 
                 // Checking if user has borrowed books
-                $select = "SELECT count(borrowed_id) AS num_borrowed FROM borrowings WHERE borrower_id=?";
+                $select = "SELECT num_borrowed, avg_rating FROM books WHERE book_id=? AND borrower_id=?";
                 $stmt = $conn->prepare($select);
-                $stmt->bind_param("i", $user_id);
+                $stmt->bind_param("ii", $book_id, $user_id);
                 $stmt->execute();
                 $result = $stmt->get_result();
                 if($result->num_rows>0) 
                 {
-                    while($row = $result->fetch_assoc())
-                    {
-                        $num_borrowed=$row["num_borrowed"];
-                        if($num_borrowed==0)
-                        {
-                            exit;
-                        }
-                    }
+                    $row = $result->fetch_assoc();
+                    $num_borrowed=$row["num_borrowed"];
+                    $avg_rating=$row["avg_rating"];
                 }
-                
+                if($num_borrowed!=0)
+                {
+                    $new_rating = (($num_borrowed*$avg_rating)+$rating)/($num_borrowed+1);
+                }
+                else
+                {
+                    $new_rating = $rating;
+                }
+                $num_borrowed = $num_borrowed + 1;
                 // Verifying that book is borrowed
                 $select = "SELECT * FROM borrowings WHERE borrowed_id=? AND borrower_id=? AND curdate()<=dor";
                 $stmt = $conn->prepare($select);
@@ -116,6 +89,7 @@
                 $result = $stmt->get_result();
                 if($result>0) 
                 {
+
                     $delete = "DELETE FROM borrowings WHERE borrowed_id=? AND borrower_id=?";
                     $stmt = $conn->prepare($delete);
                     $stmt->bind_param("ii", $book_id, $user_id);
@@ -124,6 +98,16 @@
                     $update_books = "UPDATE books SET borrower_id=NULL WHERE book_id=?";
                     $stmt = $conn->prepare($update_books);
                     $stmt->bind_param("i", $book_id);
+                    $stmt->execute();
+
+                    $update_books = "UPDATE books SET avg_rating=? WHERE book_id=?";
+                    $stmt = $conn->prepare($update_books);
+                    $stmt->bind_param("ii", $new_rating, $book_id);
+                    $stmt->execute();
+
+                    $update_books = "UPDATE books SET num_borrowed=? WHERE book_id=?";
+                    $stmt = $conn->prepare($update_books);
+                    $stmt->bind_param("ii", $num_borrowed, $book_id);
                     $stmt->execute();
 
                     echo "<p>Successfully returned!</p>";
